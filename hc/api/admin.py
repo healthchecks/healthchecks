c21b6b699b443363ebffb62cd64aca86961c0c91
Pēteris Caune <cuu508@gmail.com>
2015-07-11 13:58:15 +0300

    A bit nicer admin UI for checks

--- hc/api/admin.py
+++ hc/api/admin.py
@@ -5,8 +5,17 @@ from hc.api.models import Check
 
 @admin.register(Check)
 class ChecksAdmin(admin.ModelAdmin):
-    list_display = ("id", "code", "user", "last_ping")
+    class Media:
+        css = {
+         'all': ('css/admin/checks.css',)
+        }
+
+    list_display = ("id", "name", "created", "code", "status", "email", "last_ping")
+    list_select_related = ("user", )
     actions = ["send_alert"]
+
+    def email(self, obj):
+        return obj.user.email if obj.user else None
 
     def send_alert(self, request, qs):
         for check in qs:
