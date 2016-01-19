4558628ebf216467cf43a02154f303d5360fba8c
Di Wu <dw323@cornell.edu>
2016-01-18 21:54:20 -0800

    defer inserting the password field

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,9 @@
 $(function () {
+    var passwordInput = $("#password-block input");
+    passwordInput.detach();
     $("#password-toggle").click(function() {
         $("#password-toggle").hide();
         $("#password-block").removeClass("hide");
+        $("#password-block .input-group").append(passwordInput);
     });
-
 });
