30d976076867fd14d1d17c3e6ba553aad32135dd
Pēteris Caune <cuu508@gmail.com>
2016-09-24 21:39:09 +0300

    Don't autoselect ping address in the welcome page. It gets in the way when selecting just the code portion of the address.

--- static/js/welcome.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function() {
-
-    var input = document.getElementById("pitch-url-input");
-    input.addEventListener("click", function() {
-        this.select();
-    });
-
-}, 1000);
