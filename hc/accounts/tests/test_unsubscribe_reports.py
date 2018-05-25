7ced981d45ed01de773ac6298875809f612258e4
Pēteris Caune <cuu508@gmail.com>
2018-05-25 23:38:02 +0300

    Remove obsolete signature checking code in accounts.views.unsubscribe_reports

--- hc/accounts/tests/test_unsubscribe_reports.py
+++ hc/accounts/tests/test_unsubscribe_reports.py
@@ -7,14 +7,15 @@ from hc.test import BaseTestCase
 
 class UnsubscribeReportsTestCase(BaseTestCase):
 
-    def test_token_works(self):
+    def test_it_unsubscribes(self):
         self.profile.next_report_date = now()
         self.profile.nag_period = td(hours=1)
         self.profile.next_nag_date = now()
         self.profile.save()
 
-        token = signing.Signer().sign("foo")
-        url = "/accounts/unsubscribe_reports/alice/?token=%s" % token
+        sig = signing.TimestampSigner(salt="reports").sign("alice")
+        url = "/accounts/unsubscribe_reports/%s/" % sig
+
         r = self.client.get(url)
         self.assertContains(r, "You have been unsubscribed")
 
@@ -24,20 +25,6 @@ class UnsubscribeReportsTestCase(BaseTestCase):
 
         self.assertEqual(self.profile.nag_period.total_seconds(), 0)
         self.assertIsNone(self.profile.next_nag_date)
-
-    def test_bad_token_gets_rejected(self):
-        url = "/accounts/unsubscribe_reports/alice/?token=invalid"
-        r = self.client.get(url)
-        self.assertContains(r, "Incorrect Link")
-
-    def test_signed_username_works(self):
-        sig = signing.TimestampSigner(salt="reports").sign("alice")
-        url = "/accounts/unsubscribe_reports/%s/" % sig
-        r = self.client.get(url)
-        self.assertContains(r, "You have been unsubscribed")
-
-        self.profile.refresh_from_db()
-        self.assertFalse(self.profile.reports_allowed)
 
     def test_bad_signature_gets_rejected(self):
         url = "/accounts/unsubscribe_reports/invalid/"
