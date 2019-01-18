512c67a8f9ce87322ac46764837a9a4ae51ebcbf
Pēteris Caune <cuu508@gmail.com>
2019-01-18 16:57:49 +0200

    Fix Trello testcase.

--- hc/front/tests/test_add_trello.py
+++ hc/front/tests/test_add_trello.py
@@ -1,5 +1,6 @@
 import json
 
+from django.test.utils import override_settings
 from hc.api.models import Channel
 from hc.test import BaseTestCase
 
@@ -7,11 +8,13 @@ from hc.test import BaseTestCase
 class AddPagerTreeTestCase(BaseTestCase):
     url = "/integrations/add_trello/"
 
+    @override_settings(TRELLO_APP_KEY="foo")
     def test_instructions_work(self):
         self.client.login(username="alice@example.org", password="password")
         r = self.client.get(self.url)
         self.assertContains(r, "Trello")
 
+    @override_settings(TRELLO_APP_KEY="foo")
     def test_it_works(self):
         form = {"settings": json.dumps({
             "token": "fake-token",
