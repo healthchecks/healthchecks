e98bd42438408bf4a89edaed586491a7017dff41
Pēteris Caune <cuu508@gmail.com>
2019-01-19 18:51:29 +0200

    Fix project names in the top navigation dropdown menu.

--- hc/accounts/middleware.py
+++ hc/accounts/middleware.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from hc.accounts.models import Profile, Project
 
 
@@ -9,7 +10,9 @@ class TeamAccessMiddleware(object):
         if not request.user.is_authenticated:
             return self.get_response(request)
 
-        projects_q = Project.objects.filter(member__user_id=request.user.id)
+        is_owner = Q(owner=request.user)
+        is_member = Q(member__user_id=request.user.id)
+        projects_q = Project.objects.filter(is_owner | is_member)
         projects_q = projects_q.select_related("owner")
         request.get_projects = lambda: list(projects_q)
 
