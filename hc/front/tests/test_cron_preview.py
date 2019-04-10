12f8ffcd80a6ca663613e31ca8569aa697c4ce62
Pēteris Caune <cuu508@gmail.com>
2019-04-10 12:09:36 +0300

    Django==2.2, pytz==2019.1

--- hc/front/tests/test_cron_preview.py
+++ hc/front/tests/test_cron_preview.py
@@ -16,8 +16,7 @@ class CronPreviewTestCase(BaseTestCase):
         self.assertContains(r, "cron-preview-title", status_code=200)
 
     def test_it_rejects_invalid_cron_expression(self):
-        samples = [None,
-                   "",
+        samples = ["",
                    "*",
                    "100 100 100 100 100",
                    "* * * * * *",
@@ -29,14 +28,14 @@ class CronPreviewTestCase(BaseTestCase):
             self.assertContains(r, "Invalid cron expression", status_code=200)
 
     def test_it_handles_invalid_timezone(self):
-        for tz in [None, "", "not-a-timezone"]:
+        for tz in ["", "not-a-timezone"]:
             payload = {"schedule": "* * * * *", "tz": tz}
             r = self.client.post("/checks/cron_preview/", payload)
             self.assertContains(r, "Invalid timezone", status_code=200)
 
     def test_it_handles_missing_arguments(self):
         r = self.client.post("/checks/cron_preview/", {})
-        self.assertContains(r, "Invalid cron expression", status_code=200)
+        self.assertContains(r, "Invalid timezone", status_code=200)
 
     def test_it_rejects_get(self):
         r = self.client.get("/checks/cron_preview/", {})
