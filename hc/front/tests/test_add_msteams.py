d0b77febbc2ec85638034de905e2eac673297747
Pēteris Caune <cuu508@gmail.com>
2020-02-21 14:58:22 +0200

    Project code in URL for the "Add MS Teams" page. cc: #336

--- hc/front/tests/test_add_msteams.py
+++ hc/front/tests/test_add_msteams.py
@@ -3,17 +3,21 @@ from hc.test import BaseTestCase
 
 
 class AddMsTeamsTestCase(BaseTestCase):
+    def setUp(self):
+        super(AddMsTeamsTestCase, self).setUp()
+        self.url = "/projects/%s/add_msteams/" % self.project.code
+
     def test_instructions_work(self):
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.get("/integrations/add_msteams/")
+        r = self.client.get(self.url)
         self.assertContains(r, "Integration Settings", status_code=200)
 
     def test_it_works(self):
         form = {"value": "https://example.com/foo"}
 
         self.client.login(username="alice@example.org", password="password")
-        r = self.client.post("/integrations/add_msteams/", form)
-        self.assertRedirects(r, "/integrations/")
+        r = self.client.post(self.url, form)
+        self.assertRedirects(r, self.channels_url)
 
         c = Channel.objects.get()
         self.assertEqual(c.kind, "msteams")
