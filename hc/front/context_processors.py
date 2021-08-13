f7dd16abcab5d5e0134083267f21672de8e3d5e1
Pēteris Caune <cuu508@gmail.com>
2021-08-13 15:03:09 +0300

    Remove site_root from template context, it's never used

--- hc/front/context_processors.py
+++ hc/front/context_processors.py
@@ -4,6 +4,5 @@ from django.conf import settings
 def branding(request):
     return {
         "site_name": settings.SITE_NAME,
-        "site_root": settings.SITE_ROOT,
         "site_logo_url": settings.SITE_LOGO_URL,
     }
