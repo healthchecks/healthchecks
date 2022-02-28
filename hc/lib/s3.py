6b82f1d17fb15eb60cb1c251e81912c04d8da54a
Pēteris Caune <cuu508@gmail.com>
2022-02-28 15:36:26 +0200

    Document the trickery in s3.py

--- hc/lib/s3.py
+++ hc/lib/s3.py
@@ -28,6 +28,25 @@ ASCII_Z = ord("z")
 
 
 def enc(n):
+    """Generates an object key in the "<sorting prefix>-<n>" form.
+
+    >>> [enc(i) for i in range(0, 5)]
+    ['zj-0', 'zi-1', 'zh-2', 'zg-3', 'zf-4']
+
+    The purpose of the sorting prefix is to sort keys with smaller n values
+    last:
+
+    >>> sorted([enc(i) for i in range(0, 5)])
+    ['zf-4', 'zg-3', 'zh-2', 'zi-1', 'zj-0']
+
+    This allows efficient lookup of objects with n
+    values below a specific threshold. For example, the following
+    retrieves all keys at bucket's root directory with n < 123:
+
+    >>> client.list_objects(bucket_name, start_after=enc(123))
+
+    """
+
     s = str(n)
     len_inverted = chr(ASCII_Z - len(s) + 1)
     inverted = "".join(chr(ASCII_J - int(c)) for c in s)
@@ -65,4 +84,11 @@ def _remove_objects(code, upto_n):
 
 
 def remove_objects(check_code, upto_n):
+    """Removes keys with n values below or equal to `upto_n`.
+
+    The S3 API calls can take seconds to complete,
+    therefore run the removal code on thread.
+
+    """
+
     Thread(target=_remove_objects, args=(check_code, upto_n)).start()
