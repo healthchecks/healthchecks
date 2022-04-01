a6e52bc254f3a9c8af34e2b44bb91a04f72e0d7a
Pēteris Caune <cuu508@gmail.com>
2022-04-01 14:36:56 +0300

    Tweak wording in the "check limit reached" message

--- hc/front/tests/test_my_checks.py
+++ hc/front/tests/test_my_checks.py
@@ -84,7 +84,7 @@ class MyChecksTestCase(BaseTestCase):
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.get(self.url)
-        self.assertContains(r, "Check limit reached", status_code=200)
+        self.assertContains(r, "Time to upgrade", status_code=200)
 
     def test_it_saves_sort_field(self):
         self.client.login(username="alice@example.org", password="password")
