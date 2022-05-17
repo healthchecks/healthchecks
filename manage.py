1a41e4e1998fbb475d9a8b30c2f4bbdfa21ce655
Pēteris Caune <cuu508@gmail.com>
2022-05-17 10:29:13 +0300

    Enable error emails from management commands

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 import os
 import sys
 
@@ -7,4 +8,10 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    logger = logging.getLogger("django")
+    try:
+        execute_from_command_line(sys.argv)
+    except Exception as e:
+        msg = "Admin Command Error: %s"
+        logger.error(msg, " ".join(sys.argv), exc_info=sys.exc_info())
+        raise e
