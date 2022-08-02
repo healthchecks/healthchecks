d148e7ad3d20f4b2eb3d4bf5e381d38e31d446a8
Facorazza <20555025+facorazza@users.noreply.github.com>
2022-08-02 22:57:29 +0200

    Add support for SMTP with implicit TLS

--- hc/settings.py
+++ hc/settings.py
@@ -181,10 +181,11 @@ WHITENOISE_IMMUTABLE_FILE_TEST = immutable_file_test
 
 # SMTP credentials for sending email
 EMAIL_HOST = os.getenv("EMAIL_HOST", "")
-EMAIL_PORT = envint("EMAIL_PORT", "587")
+EMAIL_PORT = envint("EMAIL_PORT", "465")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-EMAIL_USE_TLS = envbool("EMAIL_USE_TLS", "True")
+EMAIL_USE_TLS = envbool("EMAIL_USE_TLS", "False")
+EMAIL_USE_SSL = envbool("EMAIL_USE_SSL", "False")
 EMAIL_USE_VERIFICATION = envbool("EMAIL_USE_VERIFICATION", "True")
 
 # WebAuthn
