c26dbc96f50a566e07e7d94b4b80454f9b5b8584
Pēteris Caune <cuu508@gmail.com>
2022-10-28 12:05:01 +0300

    Refactor render_docs to support multiple docs directories

--- hc/front/management/commands/render_docs.py
+++ hc/front/management/commands/render_docs.py
@@ -21,21 +21,33 @@ class Command(BaseCommand):
             self.stdout.write(f"  pip install {e.name}\n\n")
             return
 
-        extensions = ["fenced_code", "codehilite", "tables", "def_list", "attr_list"]
-        ec = {"codehilite": {"css_class": "highlight", "startinline": True}}
+        extensions = [
+            "fenced_code",
+            "codehilite",
+            "tables",
+            "def_list",
+            "attr_list",
+        ]
+        extension_configs = {
+            "codehilite": {"css_class": "highlight", "startinline": True}
+        }
 
-        docs_path = os.path.join(settings.BASE_DIR, "templates/docs")
-        for doc in os.listdir(docs_path):
-            if not doc.endswith(".md"):
-                continue
+        def process_directory(path):
+            for doc in os.listdir(path):
+                if not doc.endswith(".md"):
+                    continue
 
-            print("Rendering %s" % doc)
+                print("Rendering %s" % doc)
 
-            src_path = os.path.join(docs_path, doc)
-            dst_path = os.path.join(docs_path, doc[:-3] + ".html")
+                src_path = os.path.join(path, doc)
+                dst_path = os.path.join(path, doc[:-3] + ".html")
 
-            text = open(src_path, "r", encoding="utf-8").read()
-            html = markdown.markdown(text, extensions=extensions, extension_configs=ec)
+                text = open(src_path, "r", encoding="utf-8").read()
+                html = markdown.markdown(
+                    text, extensions=extensions, extension_configs=extension_configs
+                )
 
-            with open(dst_path, "w", encoding="utf-8") as f:
-                f.write(html)
+                with open(dst_path, "w", encoding="utf-8") as f:
+                    f.write(html)
+
+        process_directory(os.path.join(settings.BASE_DIR, "templates/docs"))
