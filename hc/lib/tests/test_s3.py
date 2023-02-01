e995d299b87bb9671281cd041dd3685f1efcb743
Pēteris Caune <cuu508@gmail.com>
2023-02-01 10:25:17 +0200

    Improve hc.lib.s3 tests

--- hc/lib/tests/test_s3.py
+++ hc/lib/tests/test_s3.py
@@ -4,6 +4,7 @@ from unittest import skipIf
 from unittest.mock import Mock, patch
 
 from django.test import TestCase
+from django.test.utils import override_settings
 
 from hc.lib.s3 import get_object
 
@@ -17,14 +18,23 @@ except ImportError:
 
 
 @skipIf(not have_minio, "minio not installed")
+@override_settings(S3_BUCKET="dummy-bucket")
 class S3TestCase(TestCase):
     @patch("hc.lib.s3._client")
     def test_get_object_handles_s3error(self, mock_client):
         e = S3Error("a", "b", "c", "d", "e", "f")
         mock_client.get_object.return_value.read = Mock(side_effect=e)
         self.assertIsNone(get_object("dummy-code", 1))
+        self.assertTrue(mock_client.get_object.called)
 
     @patch("hc.lib.s3._client")
     def test_get_object_handles_protocolerror(self, mock_client):
         mock_client.get_object.return_value.read = Mock(side_effect=ProtocolError)
         self.assertIsNone(get_object("dummy-code", 1))
+        self.assertTrue(mock_client.get_object.called)
+
+    @override_settings(S3_BUCKET=None)
+    @patch("hc.lib.s3._client")
+    def test_get_object_handles_no_s3_configuration(self, mock_client):
+        self.assertIsNone(get_object("dummy-code", 1))
+        self.assertFalse(mock_client.get_object.called)
