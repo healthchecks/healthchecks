05742f42f938bf6b9a2630caf36560c728d1d0d9
Pēteris Caune <cuu508@gmail.com>
2023-07-14 09:51:36 +0300

    Update the senddeletionscheduled command to notify team members too

--- hc/accounts/management/commands/senddeletionscheduled.py
+++ hc/accounts/management/commands/senddeletionscheduled.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import time
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 
@@ -16,23 +17,35 @@ class Command(BaseCommand):
     def pause(self):
         time.sleep(1)
 
+    def members(self, user):
+        q = User.objects.filter(memberships__project__owner=user)
+        q = q.exclude(last_login=None)
+        return q.order_by("email")
+
     def handle(self, *args, **options):
         q = Profile.objects.order_by("id")
         q = q.filter(deletion_scheduled_date__gt=now())
 
         sent = 0
         for profile in q:
-            self.stdout.write(f"Sending notice to {profile.user.email}")
+            recipients = [profile.user.email]
+            # Include team members in the recipient list too:
+            for u in self.members(profile.user):
+                recipients.append(u.email)
 
-            ctx = {
-                "email": profile.user.email,
-                "support_email": settings.SUPPORT_EMAIL,
-                "deletion_scheduled_date": profile.deletion_scheduled_date,
-            }
-            emails.deletion_scheduled(profile.user.email, ctx)
-            sent += 1
+            for recipient in recipients:
+                role = "owner" if recipient == profile.user.email else "member"
+                self.stdout.write(f"Sending notice to {recipient} ({role})")
+                ctx = {
+                    "owner_email": profile.user.email,
+                    "num_checks": profile.num_checks_used(),
+                    "support_email": settings.SUPPORT_EMAIL,
+                    "deletion_scheduled_date": profile.deletion_scheduled_date,
+                }
+                emails.deletion_scheduled(recipient, ctx)
+                sent += 1
 
-            # Throttle so we don't send too many emails at once:
-            self.pause()
+                # Throttle so we don't send too many emails at once:
+                self.pause()
 
         return f"Done!\nNotices sent: {sent}\n"
--- hc/accounts/tests/test_senddeletionscheduled.py
+++ hc/accounts/tests/test_senddeletionscheduled.py
@@ -8,6 +8,7 @@ from django.core import mail
 from django.utils.timezone import now
 
 from hc.accounts.management.commands.senddeletionscheduled import Command
+from hc.api.models import Check
 from hc.test import BaseTestCase
 
 
@@ -16,18 +17,13 @@ def counts(result):
     return [int(s) for s in re.findall(r"\d+", result)]
 
 
-class SendDeletionNoticesTestCase(BaseTestCase):
-    def test_it_skips_profiles_with_deletion_scheduled_date_not_set(self):
-        cmd = Command(stdout=Mock())
-        cmd.pause = Mock()  # don't pause for 1s
-
-        result = cmd.handle()
-        self.assertEqual(counts(result), [0])
-        self.assertEqual(len(mail.outbox), 0)
-
+class SendDeletionScheduledTestCase(BaseTestCase):
     def test_it_sends_notice(self):
         self.profile.deletion_scheduled_date = now() + td(days=31)
         self.profile.save()
+
+        Check.objects.create(project=self.project)
+        Check.objects.create(project=self.project)
 
         cmd = Command(stdout=Mock())
         cmd.pause = Mock()  # don't pause for 1s
@@ -37,6 +33,33 @@ class SendDeletionNoticesTestCase(BaseTestCase):
 
         email = mail.outbox[0]
         self.assertEqual(email.subject, "Account Deletion Warning")
+        self.assertEqual(email.to[0], "alice@example.org")
+        self.assertIn("Owner: alice@example.org", email.body)
+        self.assertIn("Number of checks in the account: 2", email.body)
+
+    def test_it_sends_notice_to_team_members(self):
+        self.profile.deletion_scheduled_date = now() + td(days=31)
+        self.profile.save()
+
+        self.bob.last_login = now()
+        self.bob.save()
+
+        cmd = Command(stdout=Mock())
+        cmd.pause = Mock()  # don't pause for 1s
+
+        result = cmd.handle()
+        self.assertEqual(counts(result), [2])
+
+        self.assertEqual(mail.outbox[0].to[0], "alice@example.org")
+        self.assertEqual(mail.outbox[1].to[0], "bob@example.org")
+
+    def test_it_skips_profiles_with_deletion_scheduled_date_not_set(self):
+        cmd = Command(stdout=Mock())
+        cmd.pause = Mock()  # don't pause for 1s
+
+        result = cmd.handle()
+        self.assertEqual(counts(result), [0])
+        self.assertEqual(len(mail.outbox), 0)
 
     def test_it_skips_profiles_with_deletion_scheduled_date_in_past(self):
         self.profile.deletion_scheduled_date = now() - td(minutes=1)
