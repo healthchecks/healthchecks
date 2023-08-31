4affd708223c200d0c89dba3c65cc94b4c572487
Pēteris Caune <cuu508@gmail.com>
2023-08-31 12:25:18 +0300

    Fix JSON type aliases for Py 3.8 again

--- hc/lib/typealias.py
+++ hc/lib/typealias.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from typing import Dict, List
+from typing import Dict, List, Union
 
 JSONDict = Dict[str, "JSONValue"]
 JSONList = List["JSONValue"]
-JSONValue = JSONDict | JSONList | str | int | float | bool | None
+JSONValue = Union[JSONDict, JSONList, str, int, float, bool, None]
