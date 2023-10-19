a308997b3a8617cbe970ffe701156265c5b8ab68
Pēteris Caune <cuu508@gmail.com>
2023-10-19 13:49:13 +0300

    Remove most of the hc.payments stuff
    Why remove:
    
    * For self-hosters, payment-related features are unused and dead weight
    * For SaaS (any would-be Healthchecks.io competitors), the existing
      payment handling logic is not very useful either, as it would need to
      be heavily modified to match their business model, pricing, chosen
      payment gateway
    * For the hosted service (Healthchecks.io), the up-to-date billing code
      lives in a private fork of this repo. Maintenance is easier if this
      repo does not have an older, diverging version of the same
      functionality
    
    A few payment-related bits are staying at least for time being:
    
    * the "USE_PAYMENTS" setting
    * the hc.payments.models.Subscription model
    * tiny stubs for the "Pricing" and "Billing" pages
    
    They are used in various places in the code and templates,
    and I think ripping them out in one go would be too disruptive.

--- hc/accounts/tests/test_close_account.py
+++ hc/accounts/tests/test_close_account.py
@@ -25,8 +25,7 @@ class CloseAccountTestCase(BaseTestCase):
         self.assertContains(r, "1 project")
         self.assertContains(r, "0 checks")
 
-    @patch("hc.payments.models.braintree")
-    def test_it_works(self, mock_braintree: Mock) -> None:
+    def test_it_works(self) -> None:
         Check.objects.create(project=self.project, tags="foo a-B_1  baz@")
         Subscription.objects.create(
             user=self.alice, subscription_id="123", customer_id="fake-customer-id"
@@ -46,9 +45,6 @@ class CloseAccountTestCase(BaseTestCase):
 
         # Check should be gone
         self.assertFalse(Check.objects.exists())
-
-        # Subscription should have been canceled
-        mock_braintree.Subscription.cancel.assert_called_once()
 
         # Subscription should be gone
         self.assertFalse(Subscription.objects.exists())
--- hc/payments/urls.py
+++ hc/payments/urls.py
@@ -8,17 +8,4 @@ urlpatterns = [
     path("projects/<uuid:code>/pricing/", views.pricing, name="hc-p-pricing"),
     path("pricing/", views.pricing, name="hc-pricing"),
     path("accounts/profile/billing/", views.billing, name="hc-billing"),
-    path(
-        "accounts/profile/billing/history/",
-        views.billing_history,
-        name="hc-billing-history",
-    ),
-    path("accounts/profile/billing/address/", views.address, name="hc-billing-address"),
-    path(
-        "accounts/profile/billing/payment_method/",
-        views.payment_method,
-        name="hc-payment-method",
-    ),
-    path("pricing/update/", views.update, name="hc-update-subscription"),
-    path("pricing/token/", views.token, name="hc-get-client-token"),
 ]
