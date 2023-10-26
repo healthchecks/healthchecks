6872ab3eb6122e5c89a286aa379adcf60505409c
Pēteris Caune <cuu508@gmail.com>
2023-10-26 09:31:11 +0300

    Improve type hints in hc.accounts.backends

--- hc/accounts/backends.py
+++ hc/accounts/backends.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.http import HttpRequest
 
 from hc.accounts.models import Profile
 from hc.accounts.views import _make_user
 
 
 class BasicBackend(object):
-    def get_user(self, user_id):
+    def get_user(self, user_id: int) -> User | None:
         try:
             q = User.objects.select_related("profile")
 
@@ -19,7 +20,15 @@ class BasicBackend(object):
 
 # Authenticate against the token in user's profile.
 class ProfileBackend(BasicBackend):
-    def authenticate(self, request=None, username=None, token=None):
+    def authenticate(
+        self,
+        request: HttpRequest,
+        username: str | None = None,
+        token: str | None = None,
+    ) -> User | None:
+        if not token:
+            return None
+
         try:
             profiles = Profile.objects.select_related("user")
             profile = profiles.get(user__username=username)
@@ -33,14 +42,24 @@ class ProfileBackend(BasicBackend):
 
 
 class EmailBackend(BasicBackend):
-    def authenticate(self, request=None, username=None, password=None):
+    def authenticate(
+        self,
+        request: HttpRequest,
+        username: str | None = None,
+        password: str | None = None,
+    ) -> User | None:
+        if not password:
+            return None
+
         try:
             user = User.objects.get(email=username)
         except User.DoesNotExist:
             return None
 
-        if user.check_password(password):
-            return user
+        if not user.check_password(password):
+            return None
+
+        return user
 
 
 class CustomHeaderBackend(BasicBackend):
@@ -50,7 +69,9 @@ class CustomHeaderBackend(BasicBackend):
 
     """
 
-    def authenticate(self, request, remote_user_email):
+    def authenticate(
+        self, request: HttpRequest, remote_user_email: str | None = None
+    ) -> User | None:
         """
         The email address passed as remote_user_email is considered trusted.
         Return the User object with the given email address. Create a new User
