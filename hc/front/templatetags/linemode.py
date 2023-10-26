db1088eb9c0888b3e7589289e785844166e82ae0
Pēteris Caune <cuu508@gmail.com>
2023-10-26 08:41:05 +0300

    Add type hints in hc.front.templatetags.linemode

--- hc/front/templatetags/linemode.py
+++ hc/front/templatetags/linemode.py
@@ -1,23 +1,24 @@
 from __future__ import annotations
 
 from django import template
-from django.template import Node, TemplateSyntaxError
+from django.template import Context, Node, NodeList, TemplateSyntaxError
+from django.template.base import Parser, Token
 
 register = template.Library()
 
 
 class LineModeNode(Node):
-    def __init__(self, nodelist):
+    def __init__(self, nodelist: NodeList) -> None:
         self.nodelist = nodelist
 
-    def render(self, context):
+    def render(self, context: Context) -> str:
         context["__lines__"] = []
         self.nodelist.render(context)
         return "\n".join(context["__lines__"])
 
 
 @register.tag
-def linemode(parser, token):
+def linemode(parser: Parser, token: Token) -> LineModeNode:
     """Skip content outside {% line %} blocks.
 
     Intended to be used for precise control of whitespace and newlines.
@@ -44,10 +45,10 @@ def linemode(parser, token):
 
 
 class LineNode(Node):
-    def __init__(self, nodelist):
+    def __init__(self, nodelist: NodeList) -> None:
         self.nodelist = nodelist
 
-    def render(self, context):
+    def render(self, context: Context) -> str:
         if "__lines__" not in context:
             raise TemplateSyntaxError("The line tag used without outer linemode tags")
 
@@ -56,7 +57,7 @@ class LineNode(Node):
 
 
 @register.tag
-def line(parser, token):
+def line(parser: Parser, token: Token) -> LineNode:
     """For use with {% linemode %}.
 
     Renders the enclosed content and appends it to context["__lines__"]
