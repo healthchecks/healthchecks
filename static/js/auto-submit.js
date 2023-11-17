decd1d4b87edcd0229984dd17d73af0d1e3445f2
Pēteris Caune <cuu508@gmail.com>
2023-11-17 11:00:41 +0200

    Improve TOTP auto-submit code
    * Listen to "input" event only, don't need "keyup"
    * Add form double-submit protection
    * Rewrite in plain JS (the page no longer loads jQuery)

--- static/js/auto-submit.js
+++ static/js/auto-submit.js
@@ -1,7 +1,15 @@
-$(function() {
-    $("input[data-auto-submit]").on("keyup input", function() {
-        if (this.validity.valid) {
+window.addEventListener("DOMContentLoaded", function(e) {
+    var validateAndSubmit = function() {
+        if (this.validity.valid && !this.dataset.submitted) {
+            // Make sure we only submit the form once
+            this.dataset.submitted = true;
             this.form.submit();
         }
+    }
+
+    // Hook up validateAndSubmit to all input elements with the
+    // "data-auto-submit" attribute
+    document.querySelectorAll("input[data-auto-submit]").forEach((input) => {
+        input.addEventListener("input", validateAndSubmit);
     });
-})
+});
