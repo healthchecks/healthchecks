a4d2094cefa2d1452e691bb23486e7f709781ae6
Pēteris Caune <cuu508@gmail.com>
2024-04-16 16:36:49 +0300

    Update the remove_project view to delete checks using lock_and_delete()
    If we delete project by naively calling project.delete() then checks
    can receive pings during the deletion, causing the deletion operation
    to fail with an IntegrityError.
    
    So instead do it like so:
    
    * iterate over project's checks, call Check.lock_and_delete() on each
    * in the end, call project.delete()

--- hc/accounts/views.py
+++ hc/accounts/views.py
@@ -720,6 +720,8 @@ def close(request: AuthenticatedHttpRequest) -> HttpResponse:
 @login_required
 def remove_project(request: AuthenticatedHttpRequest, code: str) -> HttpResponse:
     project = get_object_or_404(Project, code=code, owner=request.user)
+    for check in project.check_set.all():
+        check.lock_and_delete()
     project.delete()
     return redirect("hc-index")
 
