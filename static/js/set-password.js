5d63057e789b6fa63375dafa55854a18e139d77b
Pēteris Caune <cuu508@gmail.com>
2024-08-15 11:10:14 +0300

    Improve password quality meter for very weak passwords
    Previously, if the user enters a weak password like "qwerty",
    the score is 0, the password strength bar is empty (all gray).
    It is easy to not notice the password strength bar at all.
    
    Now, the lowest score for a non-empty password is 1, meaning
    the user will see one red bar. This will hopefully draw more
    attention to the password strength bar.
    
    Users are still allowed to choose weak passwords.

--- static/js/set-password.js
+++ static/js/set-password.js
@@ -2,7 +2,15 @@ $(function () {
     var $pw = $("#password");
     var $meter = $("#meter");
     $pw.on("input", function() {
-        var result = zxcvbn($pw.val());
-        $meter.attr("class", "score-" + result.score);
+        var candidate = $pw.val();
+        if (!candidate) {
+            $meter.attr("class", "score-0");
+            return;
+        }
+
+        var result = zxcvbn(candidate);
+        // If user has entered anything at all cap score at 1
+        var score = Math.max(result.score, 1);
+        $meter.attr("class", "score-" + score);
     });
 });
