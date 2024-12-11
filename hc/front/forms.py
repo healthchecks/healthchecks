8612abc821a7c3dad8cdbd70567bb1721d27d234
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2024-12-11 12:08:06 +0100

    update validations

--- hc/front/forms.py
+++ hc/front/forms.py
@@ -316,12 +316,11 @@ ZULIP_TARGETS = (("stream", "Stream"), ("private", "Private"))
 
 class AddZammadForm(forms.Form):
     error_css_class = "has-error"
-    topic = forms.CharField(max_length=64)
-    url = LaxURLField(max_length=1000)
-    token = forms.CharField(max_length=100, required=False)
-    priority = forms.IntegerField(initial=3, min_value=0, max_value=5)
-    priority_up = forms.IntegerField(initial=3, min_value=0, max_value=5)
-
+    url = LaxURLField(max_length=512, required=True)
+    token = forms.CharField(max_length=100, required=True)
+    customer = forms.CharField(max_length=100, required=True)
+    group = forms.CharField(max_length=60, required=False)
+    
     def get_value(self) -> str:
         return json.dumps(dict(self.cleaned_data), sort_keys=True)
 
