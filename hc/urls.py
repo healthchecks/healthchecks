46bf30ac7f7498cfd13869b3dde25aa8d4c478a9
Pēteris Caune <cuu508@gmail.com>
2024-12-19 11:15:30 +0200

    Move ^/projects/ routes from hc.urls to hc.accounts.urls

--- hc/urls.py
+++ hc/urls.py
@@ -14,18 +14,7 @@ if _path := urlparse(settings.SITE_ROOT).path.lstrip("/"):
 urlpatterns = [
     path(f"{prefix}admin/login/", accounts_views.login),
     path(f"{prefix}admin/", admin.site.urls),
-    path(f"{prefix}accounts/", include("hc.accounts.urls")),
-    path(f"{prefix}projects/add/", accounts_views.add_project, name="hc-add-project"),
-    path(
-        f"{prefix}projects/<uuid:code>/settings/",
-        accounts_views.project,
-        name="hc-project-settings",
-    ),
-    path(
-        f"{prefix}projects/<uuid:code>/remove/",
-        accounts_views.remove_project,
-        name="hc-remove-project",
-    ),
+    path(prefix, include("hc.accounts.urls")),
     path(prefix, include("hc.api.urls")),
     path(prefix, include("hc.front.urls")),
     path(prefix, include("hc.payments.urls")),
