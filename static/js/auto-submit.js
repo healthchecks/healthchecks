18585d163dbaa5a3d2ccbfd5521367c7113b28e1
Pēteris Caune <cuu508@gmail.com>
2025-03-14 10:43:58 +0200

    Improve double-submit prevention in the TOTP form
    Chrome users could submit the form multiple time by entering the TOTP
    code and then pressing Enter key, or clicking the onscreen submit
    button. This would be less likely on low latency connections (because
    auto-submit would complete before the user presses Enter key) but quite
    doable on high latency connections.
    
    The fix is to attach an event handler to form's "onsubmit" events.
    The handler lets the first event through, and call preventDefault()
    on subsequent events. This takes care of:
    
    * programmatic submits by calling form.requestSubmit()
    * Enter key in an input field
    * click on the onscreen submit button

--- static/js/auto-submit.js
+++ static/js/auto-submit.js
@@ -1,15 +1,26 @@
-window.addEventListener("DOMContentLoaded", function(e) {
-    var validateAndSubmit = function() {
-        if (this.validity.valid && !this.dataset.submitted) {
-            // Make sure we only submit the form once
-            this.dataset.submitted = true;
-            this.form.submit();
+window.addEventListener("DOMContentLoaded", function (e) {
+    // Event handler for input's oninput event
+    var validateAndSubmit = function (e) {
+        if (this.validity.valid) {
+            // Use requestSubmit() instead of submit() because submit()
+            // does not generate the onsubmit event.
+            this.form.requestSubmit();
         }
-    }
+    };
+
+    // Event handler for form's onsubmit event
+    var checkDoubleSubmit = function (e) {
+        if (this.dataset.submitted) {
+            e.preventDefault();
+        }
+
+        this.dataset.submitted = true;
+    };
 
     // Hook up validateAndSubmit to all input elements with the
     // "data-auto-submit" attribute
     document.querySelectorAll("input[data-auto-submit]").forEach((input) => {
         input.addEventListener("input", validateAndSubmit);
+        input.form.addEventListener("submit", checkDoubleSubmit);
     });
 });
