26d8af52d7aaf8147e7936717ce25ce09396cef7
Pēteris Caune <cuu508@gmail.com>
2026-07-06 10:58:56 +0300

    Fix code comment

--- hc/accounts/forms.py
+++ hc/accounts/forms.py
@@ -43,8 +43,8 @@ class SignupForm(forms.Form):
             raise forms.ValidationError("Address is too long.")
         # When user signs up with an email address that already has an account
         # we send them the magic login link. Hence we must rate-limit attempts
-        # to sign in with a specific email address the same as we would in
-        # the login form:
+        # to sign up with a specific email address the same as we would rate-limit
+        # attempts to log in with that email address:
         if not TokenBucket.authorize_login_email(v):
             raise forms.ValidationError("Too many attempts, please try later.")
 
