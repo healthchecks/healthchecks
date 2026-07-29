5b104a3a93406c86477826f246abfb707f6f5fee
Pēteris Caune <cuu508@gmail.com>
2026-07-29 13:18:27 +0300

    Simplify "True if ... else False"

--- hc/front/views.py
+++ hc/front/views.py
@@ -1145,7 +1145,7 @@ def badges(request: AuthenticatedHttpRequest, code: UUID) -> HttpResponse:
 
         fmt = form.cleaned_data["fmt"]
         states = form.cleaned_data["states"]
-        with_late = True if states == "3" else False
+        with_late = states == "3"
         if form.cleaned_data["target"] == "all":
             label = settings.MASTER_BADGE_LABEL
             url = get_badge_url(project.badge_key, "*", fmt, with_late)
