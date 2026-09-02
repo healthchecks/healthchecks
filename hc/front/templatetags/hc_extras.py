69dbd2a0801dfbcfcd554af8cbbdaaab0b39e2a3
Pēteris Caune <cuu508@gmail.com>
2026-09-02 14:14:46 +0300

    Fix type warning

--- hc/front/templatetags/hc_extras.py
+++ hc/front/templatetags/hc_extras.py
@@ -78,7 +78,10 @@ def absolute_site_logo_url() -> str:
 
 @register.filter
 def mangle_link(s: str) -> SafeString:
-    escaped = escape(s)
+    # escape() returns SafeString, but SafeString.replace() returns str.
+    # To avoid flip-flopping between str and SafeString in the loop, convert it to
+    # str in the beginning, and mark_safe() it at the very end.
+    escaped = str(escape(s))
     for fragment in (".", "://"):
         escaped = escaped.replace(fragment, f"<span>{fragment}</span>")
 
